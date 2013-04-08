e62809deb104545f321d39b9c41ccad0e25783db
Mike Gruszczynski <mikegruz@huskers.unl.edu>
2013-04-08 08:42:55 -0500

    modified:   R/kripp.boot.R

--- R/kripp.boot.R
+++ R/kripp.boot.R
@@ -1,6 +1,5 @@
 
-kripp.boot <- function(df, raters='rows', probs=c(.025,.975), 
-					   iter=100, method='nominal') {
+kripp.boot <- function(df, raters='rows', probs=c(.025,.975), iter=100, method='nominal') {
     
 	if (!is.numeric(matrix(df))) {
 			stop(df, ' contains non-numeric cells')
