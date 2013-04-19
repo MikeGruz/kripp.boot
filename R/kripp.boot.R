425fb31c34da607665de5c67ea785b919ad9abce
Mike Gruszczynski <mikegruz@huskers.unl.edu>
2013-04-19 16:40:42 -0500

    modified:   R/kripp.boot.R 	new file:   R/print.kripp.boot.R

--- R/kripp.boot.R
+++ R/kripp.boot.R
@@ -26,13 +26,3 @@ kripp.boot <- function(x, raters='rows', probs=c(.025,.975), iter=100, method='n
     class(boot.stats) <- 'kripp.boot'
     return(boot.stats)
 }
-
-
-print.kripp.boot <- function(boot.stats) {
-    cat("Bootstrapped Krippendorff's Alpha","\n\n")
-    cat('Alpha Levels for', boot.stats$raters,'raters,', 
-        boot.stats$iter, ' iterations', '\n')
-    cat('     ', boot.stats$probs[1],'= ', boot.stats$lower, '\n')
-    cat(' Mean alpha = ', boot.stats$mean.alpha, '\n')
-    cat('     ', boot.stats$probs[2],'= ', boot.stats$upper, '\n')
-}
